ed3b5560dca6f1f98ad36f0f7da4a161bc774be1
Alijonov <alijonov.me@gmail.com>
2026-04-05 10:30:08 +0500

    feat: Asosiy ma'lumotlar bazasi modellari va migratsiyalar yaratildi

---
+++ apps/audit/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class ActionType(models.TextChoices):
+    CREATE = 'create', 'Create'
+    UPDATE = 'update', 'Update'
+    DELETE = 'delete', "Delete"
+    CONFIRM = 'confirm', 'Confirm'
+
+
+class AuditLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='audit_logs')
+    action = models.CharField(max_length=50, choices=ActionType.choices)
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
+
+    table_name = models.CharField(max_length=50)
+    record_id = models.PositiveIntegerField()
+
+    old_values = models.JSONField(null=True, blank=True)
+    new_values = models.JSONField(null=True, blank=True)
+
+    timestamp = models.DateTimeField(auto_now_add=True)
---
+++ apps/projects/models.py
@@ -0,0 +1,77 @@
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from apps.users.models import Role
+
+User = get_user_model()
+
+
+class Status(models.TextChoices):
+    TODO = 'todo', 'To Do'
+    IN_PROGRESS = 'in_progress', 'In Progress'
+    DONE = 'done', 'Done'
+    CHECKED = 'checked', 'Checked'
+    PRODUCTION = 'production', 'Production'
+
+
+class Priority(models.TextChoices):
+    LOW = 'low', 'Low'
+    MEDIUM = 'medium', 'Medium'
+    HIGH = 'high', 'High'
+    CRITICAL = 'critical', 'Critical'
+
+
+class Type(models.TextChoices):
+    BUG = 'bug', 'Bug'
+    EXTRA = 'extra', 'Extra'
+    FEATURE = 'feature', 'Feature'
+    RESEARCH = 'research', 'Research'
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='manager_projects')
+    employees = models.ManyToManyField(User, related_name='employee_projects', limit_choices_to={'role': Role.EMPLOYEE})
+    auditors = models.ManyToManyField(User, related_name='audited_projects', limit_choices_to={'role': Role.AUDITOR})
+
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.TODO)
+    priority = models.CharField(max_length=20, choices=Priority.choices, default=Priority.MEDIUM)
+    type = models.CharField(max_length=20, choices=Type.choices, default=Type.FEATURE)
+
+    assignee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='tasks',
+                                 limit_choices_to={'role': Role.EMPLOYEE})
+
+    deadline = models.DateTimeField()
+    estimated_hours = models.FloatField(default=0.0)
+    actual_hours = models.FloatField(default=0.0)
+    reopened_count = models.PositiveIntegerField(default=0)
+
+    def clean(self):
+        super().clean()
+
+        if self.assignee and self.project:
+            if not self.project.employees.filter(id=self.assignee.id).exists():
+                raise ValidationError({
+                    'assignee': "This employee has not joined his current team!"
+                })
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+
+class TaskAttachment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='attachments')
+    file = models.FileField(upload_to='task_files/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
