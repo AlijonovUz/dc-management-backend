5865f4e42d0ca2f9fd33f80aeeb771e770d23df1
Alijonov <alijonov.me@gmail.com>
2026-04-24 17:49:01 +0500

    Sozlamalar bo'limidagi celery beat sozlamalari to'g'rilandi

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,8 @@ CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 
 # Celery
-CELERY_TIMEZONE = "Asia/Tashkent"
+CELERY_TIMEZONE = 'Asia/Tashkent'
+CELERY_ENABLE_UTC = False
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
@@ -155,7 +156,7 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': crontab(hour=9, minute=0),
     },
 
-    'calculate-monthly-salaries-on-1st': {
+    'calculate-monthly-salaries-on-4th': {
         'task': 'apps.finance.tasks.calculate_monthly_salaries',
         'schedule': crontab(day_of_month=1, hour=0, minute=1),
     },
